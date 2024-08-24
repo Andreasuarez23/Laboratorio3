b4ecec707edffb01871986eefa6d30956c05ad90
Mijaliv <mijal.nunez@itscordoba.edu.ar>
2024-08-24 17:15:25 -0300

    toma de caracteres unicamente letras

--- UDPcliente.py
+++ UDPcliente.py
@@ -6,17 +6,28 @@ serverPort = 12000
 try:
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.settimeout(5)
+    while True:
+        message = input('Escriba una frase en minúsculas: ')
 
-    message = input('Escriba una frase en minúsculas: ')
+        if not message:
+            print("Error: El mensaje no puede estar vacío")
+            continue
+    
+        letras = True
+        for i in message:
+            if not ("a" <= i <= "z" or "A" <= i <= "Z"):
+                letras = False
+                
+        if not letras:
+            print("Error el mensaje solo puede contener letras")
+            continue
 
-    if not message:
-        raise ValueError("El mensaje no puede estar vacío")
+        clientSocket.sendto(message.encode(), (serverName, serverPort))
 
-    clientSocket.sendto(message.encode(), (serverName, serverPort))
+        modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
 
-    modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+        print('Respuesta del servidor:', modifiedMessage.decode())
 
-    print('Respuesta del servidor:', modifiedMessage.decode())
 
 except timeout:
     print("Error: Tiempo de espera agotado. El servidor no responde.")
